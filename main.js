b623e3fbaf9868acd3d285448cd163d4e40d6955
cccheck <hgoergens@web.de>
2020-09-26 15:44:41 +0200

    basics

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("init")
+
+var game = {};
+
+function initGame()
+{
+  //state
+  game.state = {};
+
+  //display
+  game.display = {};
+
+  //input
+  game.state = {};
+  }
+
+  game.tick = function()
+  {
+    
+  }
+}
