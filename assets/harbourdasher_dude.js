6aaee7143bed171102b8731fb25c68df0e592e9c
cccheck <hgoergens@web.de>
2020-09-27 01:21:28 +0200

    new anim

--- assets/harbourdasher_dude.js
+++ assets/harbourdasher_dude.js
@@ -1,129 +1,154 @@
 var spritesheet_1 = { "frames": {
-   "char_stand": {
-    "frame": { "x": 0, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 0.aseprite": {
+    "frame": { "x": 0, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 }
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
+    "duration": 400
    },
-   "char_run_1": {
-    "frame": { "x": 17, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 1.aseprite": {
+    "frame": { "x": 16, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_2": {
-    "frame": { "x": 34, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 2.aseprite": {
+    "frame": { "x": 32, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_3": {
-    "frame": { "x": 51, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 3.aseprite": {
+    "frame": { "x": 48, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_4": {
-    "frame": { "x": 68, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 4.aseprite": {
+    "frame": { "x": 64, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_5": {
-    "frame": { "x": 85, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 5.aseprite": {
+    "frame": { "x": 80, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_6": {
-    "frame": { "x": 102, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 6.aseprite": {
+    "frame": { "x": 96, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_7": {
-    "frame": { "x": 119, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 7.aseprite": {
+    "frame": { "x": 112, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_8": {
-    "frame": { "x": 136, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 8.aseprite": {
+    "frame": { "x": 128, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_9": {
-    "frame": { "x": 153, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 9.aseprite": {
+    "frame": { "x": 144, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_10": {
-    "frame": { "x": 170, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 10.aseprite": {
+    "frame": { "x": 160, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_11": {
-    "frame": { "x": 187, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 11.aseprite": {
+    "frame": { "x": 176, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_run_0": {
-    "frame": { "x": 204, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 12.aseprite": {
+    "frame": { "x": 192, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_break_1": {
-    "frame": { "x": 221, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 13.aseprite": {
+    "frame": { "x": 208, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_break_2": {
-    "frame": { "x": 238, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 14.aseprite": {
+    "frame": { "x": 224, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    },
-   "char_break_3": {
-    "frame": { "x": 255, "y": 0, "w": 17, "h": 15 },
+   "harbourdasher_dude 15.aseprite": {
+    "frame": { "x": 240, "y": 0, "w": 16, "h": 16 },
     "rotated": false,
     "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 17, "h": 15 },
-    "sourceSize": { "w": 17, "h": 15 },
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
+    "duration": 100
+   },
+   "harbourdasher_dude 16.aseprite": {
+    "frame": { "x": 256, "y": 0, "w": 16, "h": 16 },
+    "rotated": false,
+    "trimmed": false,
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
+    "duration": 100
+   },
+   "harbourdasher_dude 17.aseprite": {
+    "frame": { "x": 272, "y": 0, "w": 16, "h": 16 },
+    "rotated": false,
+    "trimmed": false,
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
+    "duration": 400
+   },
+   "harbourdasher_dude 18.aseprite": {
+    "frame": { "x": 288, "y": 0, "w": 16, "h": 16 },
+    "rotated": false,
+    "trimmed": false,
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 16, "h": 16 },
+    "sourceSize": { "w": 16, "h": 16 },
     "duration": 100
    }
  },
@@ -132,7 +157,7 @@ var spritesheet_1 = { "frames": {
   "version": "1.2.25",
   "image": "harbourdasher_dude.png",
   "format": "RGBA8888",
-  "size": { "w": 272, "h": 15 },
+  "size": { "w": 304, "h": 16 },
   "scale": "1",
   "frameTags": [
   ],
